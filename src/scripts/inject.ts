db7a3a0fead2c7968e08177f0944078aeb6d6894
Gaeulbyul <aeiou256@gmail.com>
2019-08-03 21:58:00 +0900

    입력칸의 최소높이 낮춤

--- src/scripts/inject.ts
+++ src/scripts/inject.ts
@@ -218,7 +218,7 @@
       display: 'block',
       width: '100%',
       height: '100%',
-      minHeight: '6rem',
+      minHeight: '1rem',
       maxHeight: '30rem',
       padding: '0',
       border: '0',
