781e6036cd5fd6cc7de061d9eb29b0231f4e50e1
Gaeulbyul <aeiou256@gmail.com>
2019-10-20 17:17:24 +0900

    에모지 글꼴로 Twemoji를 적용함

--- src/scripts/script.ts
+++ src/scripts/script.ts
@@ -12,10 +12,29 @@ async function injectScript(src: string) {
 async function loadScripts() {
   const scripts = browser.runtime.getManifest().web_accessible_resources!
   for (const path of scripts) {
+    if (!path.endsWith('.js')) {
+      continue
+    }
     const src = browser.runtime.getURL(path)
     await injectScript(src)
   }
 }
+
+function loadTwemoji() {
+  const twemojiUrl = browser.runtime.getURL('/vendor/twemoji-mozilla.ttf')
+  const css = `\
+    @font-face {
+      font-family: 'Twemoji';
+      src: url('${twemojiUrl}') format('truetype');
+    }`
+  document.body.appendChild(
+    Object.assign(document.createElement('div'), {
+      innerHTML: `&shy;<style>${css}</style>`,
+    })
+  )
+}
+
 if (document.getElementById('react-root')) {
+  loadTwemoji()
   loadScripts()
 }
